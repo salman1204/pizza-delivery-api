4a0c9623adca88e73f8e885ea12f118945ad8336
salman1204 <salmanvai38@gmail.com>
2022-04-05 22:26:01 +0600

    setup

---
+++ orders/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
+
+# Create your views here.
+class HelloOrderView(generics.GenericAPIView):
+    def get(self,request):
+        return Response(data={"message": 'Hello Order'}, status=status.HTTP_200_OK)
+
